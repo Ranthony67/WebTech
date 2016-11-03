eadd3a49320c46e9c992974a76c530d2e18ad3ae
Alexander Kledal <201371052@iha.dk>
2016-11-03 10:31:18 +0100

    add index view and use pug

--- opgave1/app.js
+++ opgave1/app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+// Set PUG as template engine
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res) {
-  res.send("Hello root");
+  res.render('views/index', { message: 'test', title: 'test' });
 });
 
 app.listen(3000, function() {
