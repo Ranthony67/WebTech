5eef6eb910fae8e239f6fd74fcc2c456d08c91d0
Alexander Kledal <201371052@iha.dk>
2016-11-03 10:37:15 +0100

    setup mongodb

--- opgave1/app.js
+++ opgave1/app.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var app = express();
 
+// MongoDB setup with mongoose
+var mongoDbPath = 'mongodb://localhost/opgave1';
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+
 // Set PUG as template engine
 app.set('view engine', 'pug');
 
+// Setup routes
 app.get('/', function(req, res) {
   res.render('index', { message: 'test', title: 'test' });
 });
 
+// Connect db
+db.on('error', console.error);
+db.on('open', function() {
+});
+
+mongoose.connect(mongoDbPath);
+
+// Initialise app server
 app.listen(3000, function() {
   console.log("App is now running on port 3000");
 });
