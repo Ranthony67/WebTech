3009ef8554ade7dd0ec59b5817c11cfd2b40af93
Alexander Kledal <201371052@iha.dk>
2016-11-23 20:16:36 +0100

    fix headers

--- opgave2/index.js
+++ opgave2/index.js
@@ -11,8 +11,9 @@ app.use(bodyParser.json({extended: true}));
 
 // Fixes CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Token, Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Token, Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
   next();
 });
 
